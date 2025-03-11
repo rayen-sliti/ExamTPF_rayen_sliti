9edf2417030950a90c3a07309583bb83afd6d3c7
rayen sliti <slitirayen50@gmail.com>
2025-03-11 01:50:27 +0100

    Squash merge feature_list into main

--- main.py
+++ main.py
@@ -10,8 +10,9 @@ som = somme(my_list)
 print('la somme est =', som)
 =======
 my_list = [1, 5, 7]
-som = sum(my_list)
-print('la somme est:', som)
+print('la somme est:', sum(my_list))
+print('le max est:', max(my_list))
+print('le min est:', min(my_list))
 
 
 >>>>>>> feature_list
